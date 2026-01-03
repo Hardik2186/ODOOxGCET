d6530c0cb2f07ba1472baba22861045f575607ca
Jagdish2401 <jagdishhadiyal2401@gmail.com>
2026-01-03 12:25:34 +0530

    feat: restore AdminDashboard import and add employee routes in App component

--- dayflow/dayflow-frontend/src/App.jsx
+++ dayflow/dayflow-frontend/src/App.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-//import AdminDashboard from "./pages/AdminDashboard";
+import AdminDashboard from "./pages/AdminDashboard";
+import EmployeeDashboard from "./pages/EmployeeDashboard";
+import EmployeeProfile from "./pages/employee/Profile";
+import EmployeeAttendance from "./pages/employee/Attendance";
+import EmployeeLeave from "./pages/employee/Leave";
+import EmployeeSalary from "./pages/employee/Salary";
 
 function App() {
   return (
@@ -11,6 +16,14 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/admin" element={<AdminDashboard />} />
+        
+        {/* Employee Routes */}
+        <Route path="/employee/dashboard" element={<EmployeeDashboard />} />
+        <Route path="/employee/profile" element={<EmployeeProfile />} />
+        <Route path="/employee/attendance" element={<EmployeeAttendance />} />
+        <Route path="/employee/leaves" element={<EmployeeLeave />} />
+        <Route path="/employee/salary" element={<EmployeeSalary />} />
+        
         <Route path="*" element={<Login />} /> {/* Default route */}
       </Routes>
     </Router>
@@ -18,3 +31,4 @@ function App() {
 }
 
 export default App;
+
