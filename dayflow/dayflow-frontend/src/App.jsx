69a58499894592d3e61dd8aa18ad88bd9c11bf99
Jagdish2401 <jagdishhadiyal2401@gmail.com>
2026-01-03 10:24:44 +0530

    feat: implement routing for Login, Signup, pages

--- dayflow/dayflow-frontend/src/App.jsx
+++ dayflow/dayflow-frontend/src/App.jsx
@@ -1,8 +1,19 @@
-import AdminDashboard from './pages/AdminDashboard';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import AdminDashboard from "./pages/AdminDashboard";
 
 function App() {
   return (
-    <AdminDashboard />
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="*" element={<Login />} /> {/* Default route */}
+      </Routes>
+    </Router>
   );
 }
 
